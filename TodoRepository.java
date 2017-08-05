2f60ba855e310671f965caf70f5febf47665d8f4
getmubarak <getmub@gmail.com>
2017-08-05 12:40:10 +0530

    Update TodoRepository.java

--- TodoRepository.java
+++ TodoRepository.java
@@ -1,12 +1,8 @@
-import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demoapp.entity.Todo;
 
 public interface TodoRepository extends MongoRepository<Todo, String> {
 
     public Todo findByTitle(String title);
-    public List<Todo> findByDesc(String title);
 
 }
