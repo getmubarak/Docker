a1ef84d09068baaf553bb3e6c9d4a9d5bb677bc7
getmubarak <getmub@gmail.com>
2017-08-05 12:41:56 +0530

    Update TodoController.java

--- TodoController.java
+++ TodoController.java
@@ -1,9 +1,6 @@
 
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +14,9 @@ public class TodoController {
 	 public TodoRepository todoRepository;
 	 
 	 @RequestMapping("/create")
-	 public Map<String, Object> create(Todo todo) {
+	 public String create(Todo todo) {
 	  todo = todoRepository.save(todo);
-	  Map<String, Object> dataMap = new HashMap<String, Object>();
-	  dataMap.put("message", "Booking created successfully");
-	  dataMap.put("status", "1");
-	  dataMap.put("booking", todo);
-	     return dataMap;
+	  return "Todo created successfully";
 	 }
 	 
 	@RequestMapping("/all")
