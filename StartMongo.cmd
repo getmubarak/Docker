16a31264d1ebcd94df28343540edd0ce6dc1b283
getmubarak <getmub@gmail.com>
2017-08-06 20:56:04 +0530

    Update StartMongo.cmd

--- StartMongo.cmd
+++ StartMongo.cmd
@@ -1,1 +1,11 @@
+#list all network
+docker network ls
+
+#create network
+docker network create spring_demo_net  
+
+#start
 docker run --name spring-demo-mongo --network=spring_demo_net -v /home/ubuntu/mongo-data:/data/db -p 27017:27017 -d mongo  
+
+#get ip address
+docker inspect --format='{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
