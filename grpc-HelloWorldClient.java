40642c7d8ea6ad7d542064657010b151aa8afca7
getmubarak <getmub@gmail.com>
2017-08-21 16:15:27 +0530

    Create grpc-HelloWorldClient.java

---
+++ grpc-HelloWorldClient.java
@@ -0,0 +1,51 @@
+package com.example.demoapp;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.examples.helloworld.HelloReply;;
+
+public class HelloWorldClient {
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final ManagedChannel channel;
+    private GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public HelloWorldClient(String hostname, int port) {
+        channel = ManagedChannelBuilder.forAddress(hostname, port)
+                .usePlaintext(true)
+                .build();
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void greet(String name) {
+        logger.info("Trying to greet " + name);
+        try {
+            HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+            HelloReply response = blockingStub.sayHello(request);
+            logger.info("Response: " + response.getMessage());
+        } catch (RuntimeException e) {
+            logger.log(Level.WARNING, "Request to grpc server failed", e);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 42420);
+        String name = args.length > 0 ? args[0] : "jack";
+
+        try {
+            client.greet(name);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
